076849a1f2fd98634a6570d4daf614469b55d904
rkamio <rkamio@rkamionoMacBook-Pro.local>
2019-12-29 02:51:28 +0900

    Mock-up

---
+++ components/OperationHistoryList.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container>
+    <v-list three-line>
+      <template v-for="(item, index) in items">
+        <v-divider v-if="index !== 0 && index !== items.lnegth - 1" :key="index"></v-divider>
+        <v-list-item :key="item.title" @click="">
+          <v-list-item-content>
+            <v-list-item-title v-html="item.title"></v-list-item-title>
+            <v-list-item-subtitle v-html="item.subtitle"></v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: "Brunch this weekend?",
+          subtitle:
+            "<span class='text--primary'>Ali Connors</span> &mdash; I'll be in your neighborhood doing errands this weekend. Do you want to hang out?"
+        },
+        {
+          title: 'Summer BBQ <span class="grey--text text--lighten-1">4</span>',
+          subtitle:
+            "<span class='text--primary'>to Alex, Scott, Jennifer</span> &mdash; Wish I could come, but I'm out of town this weekend."
+        },
+        {
+          title: "Oui oui",
+          subtitle:
+            "<span class='text--primary'>Sandra Adams</span> &mdash; Do you have Paris recommendations? Have you ever been?"
+        },
+        {
+          title: "Birthday gift",
+          subtitle:
+            "<span class='text--primary'>Trevor Hansen</span> &mdash; Have any ideas about what we should get Heidi for her birthday?"
+        },
+        {
+          title: "Recipe to try",
+          subtitle:
+            "<span class='text--primary'>Britta Holt</span> &mdash; We should eat this: Grate, Squash, Corn, and tomatillo Tacos."
+        }
+      ]
+    };
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,92 +1,268 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-container fluid v-touch:swipe.left="() => { if($vuetify.breakpoint.xs) $router.push('/operation-history') }">
+    <v-row justify="start" align="center" style="height:50px;">
+      <h3>計算欄</h3>
+      <v-spacer></v-spacer>
+      <h3>{{ fomula }}</h3>
+      <v-row class="ma-2" justify="end" align="center">
+        <v-btn right @click="backSpace()">
+          <span class="font-weight-bold display-1">⬅︎</span>
+        </v-btn>
+      </v-row>
+    </v-row>
+    <v-row align="center">
+      <v-divider></v-divider>
+    </v-row>
+    <v-row justify="start" align="center" style="height:10px;">
+      <h3>変換結果</h3>
+      <v-spacer></v-spacer>
+      <h3>{{ fomula }}</h3>
+      <v-row class="ma-2" justify="end">
+        <!-- <v-dialog v-model="selectUnitdialog" scrollable max-width="300px" :fullscreen="$vuetify.breakpoint.xs"> -->
+        <v-dialog v-model="selectUnitdialog" scrollable max-width="700px" :fullscreen="$vuetify.breakpoint.xs">
+          <template v-slot:activator="{ on }">
+            <v-btn large outlined right v-on="on">変換</v-btn>
+          </template>
+          <v-card>
+            <v-card-title>単位を選択してください</v-card-title>
+            <v-divider></v-divider>
+            <v-card-text style="height: 300px;">
+              <UnitList/>
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn color="blue darken-1" text @click="selectUnitdialog = false" >Close</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
+    </v-row>
+
+    <v-row align="end" justify="center" style="height:100px;">
+      <p class="error--text">{{ errorMessage }}</p>
+    </v-row>
+
+    <v-row align="end" justify="center" style="height:430px;">
+      <table>
+        <tr>
+          <td>
+            <div>
+              <v-btn color="error" x-large fab dark @click="clear()">
+                <span class="font-weight-bold display-1">CA</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="pink" x-large fab dark @click="updateFomula('(')">
+                <span class="font-weight-bold display-1">(</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="pink" x-large fab dark @click="updateFomula(')')">
+                <span class="font-weight-bold display-1">)</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn x-large fab dark @click="updateFomula('÷')">
+                <span class="font-weight-bold display-1">÷</span>
+              </v-btn>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('7')">
+                <span class="font-weight-bold display-1">7</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('8')">
+                <span class="font-weight-bold display-1">8</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('9')">
+                <span class="font-weight-bold display-1">9</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn x-large fab dark @click="updateFomula('×')">
+                <span class="font-weight-bold display-1">×</span>
+              </v-btn>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('4')">
+                <span class="font-weight-bold display-1">4</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('5')">
+                <span class="font-weight-bold display-1">5</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('6')">
+                <span class="font-weight-bold display-1">6</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn x-large fab dark @click="updateFomula('-')">
+                <span class="font-weight-bold display-1">−</span>
+              </v-btn>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('1')">
+                <span class="font-weight-bold display-1">1</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('2')">
+                <span class="font-weight-bold display-1">2</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('3')">
+                <span class="font-weight-bold display-1">3</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn x-large fab dark @click="updateFomula('+')">
+                <span class="font-weight-bold display-1">+</span>
+              </v-btn>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('0')">
+                <span class="font-weight-bold display-1">0</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('00')">
+                <span class="font-weight-bold display-1">00</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="warning" x-large fab dark @click="updateFomula('.')">
+                <span class="font-weight-bold display-1">.</span>
+              </v-btn>
+            </div>
+          </td>
+          <td>
+            <div>
+              <v-btn color="teal" x-large fab dark @click="calculate()">
+                <span class="font-weight-bold display-1">=</span>
+              </v-btn>
+            </div>
+          </td>
+        </tr>
+      </table>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
+import UnitList from "~/components/UnitList.vue";
 export default {
+  data() {
+    return {
+      selectUnitdialog: false,
+      fomula: '',
+      errorMessage: '',
+    };
+  },
+  methods: {
+    updateFomula: function(expression) {
+      if (this.fomula==='エラー') {
+        this.clear();
+      }
+      this.fomula+=expression;
+      console.log(this.fomula);
+    },
+    parseFomula: function(expression) {
+      let fomula = this.fomula.replace(/÷/g,'/');
+      const parsedFomula = fomula.replace(/×/g,'*');
+      return parsedFomula;
+    },
+    calculate: function() {
+      try {
+        const parsedFomula = this.parseFomula(this.fomula);
+        console.log(parsedFomula);
+        const result = eval(parsedFomula);
+        this.fomula = result + '';
+      }
+      catch (e) {
+        console.error(e);
+        this.fomula='エラー';
+      }
+    },
+    backSpace: function() {
+      this.fomula = this.fomula.slice(0,-1);
+    },
+    clear: function() {
+      this.fomula='';
+    }
+  },
+  watch: {
+    fomula: function(value) {
+      try {
+        const parsedFomula = this.parseFomula(this.fomula);
+        const result = eval(parsedFomula);
+        this.errorMessage = ''
+      }
+      catch (e) {
+        console.error(e);
+        this.errorMessage = '計算式が正しくありません。'
+      }
+    }
+  },
   components: {
-    Logo,
-    VuetifyLogo
+    UnitList
   }
-}
+};
 </script>
+
+<style>
+table {
+  border-collapse: separate;
+  border-spacing: 10px 10px;
+}
+</style>
