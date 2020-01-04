147066977dd5895c63c310c84925754a4c767554
rkamio <rkamio@rkamionoMacBook-Pro.local>
2020-01-04 23:00:59 +0900

    Release version 0.1.0

--- nuxt.config.js
+++ nuxt.config.js
@@ -88,4 +88,18 @@ export default {
     extend (config, ctx) {
     }
   },
+  /*
+  ** 
+  */
+  env: {
+    USER_FAV_MAX: process.env.USER_FAV_MAX,
+    API_KEY: process.env.API_KEY,
+    AUTH_DOMAIN: process.env.AUTH_DOMAIN,
+    DB_URL: process.env.DB_URL,
+    PROJECT_ID: process.env.PROJECT_ID,
+    STORAGE_BUCKET: process.env.STORAGE_BUCKET,
+    MESSAGING_SENDER_ID: process.env.MESSAGING_SENDER_ID,
+    APP_ID: process.env.APP_ID,
+    MESUREMENT_ID: process.env.MESUREMENT_ID
+  }
 }
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -3,17 +3,17 @@ import 'firebase/app'
 
 // .env に設定した値を取得
 const config = {
-  apiKey: process.env.NUXT_ENV_API_KEY,
-  authDomain: process.env.NUXT_ENV_AUTH_DOMAIN,
-  databaseURL: process.env.NUXT_ENV_DB_URL,
-  projectId: process.env.NUXT_ENV_PROJECT_ID,
-  storageBucket: process.env.NUXT_ENV_STORAGE_BUCKET,
-  messagingSenderId: process.env.NUXT_ENV_MESSAGING_SENDER_ID,
-  appId: process.env.NUXT_ENV_APP_ID,
-  measurementId: process.env.NUXT_ENV_MESUREMENT_ID
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MESUREMENT_ID
 }
 
-console.log(process.env.NUXT_ENV_PROJECT_ID);
+console.log(process.env.PROJECT_ID);
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config)
